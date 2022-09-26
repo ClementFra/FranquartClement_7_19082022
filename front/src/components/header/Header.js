d32898f79ebd1b0e58a77f696db7d07b2d13ba2f
ClementFra <franquart.clemnt@gmail.com>
2022-09-26 16:26:10 +0200

    header correction

--- front/src/components/header/Header.js
+++ front/src/components/header/Header.js
@@ -1,25 +1,62 @@
- import React from "react";
- import { NavLink } from "react-router-dom";
- import Logo from "../../images/logo.png";
- import "./Header.css";
- import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
- import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import Logo from "../../images/logo.png";
+import "./Header.css";
+import {
+  faRightToBracket,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
- const NavBar = () => {
+const Header = () => {
   return (
-     <header>
-       <div className="logo">
-         <NavLink to="/">
-           <img src={Logo} alt="logo de groupomania" />
+    <header>
+      <div className="logo">
+        <NavLink to="/">
+          <img src={Logo} alt="logo de groupomania" />
         </NavLink>
-         <div className="loginIcon">
-           <NavLink to="/login">
-            <FontAwesomeIcon  className="loginIcon" icon={faRightToBracket} />
+        <div className="loginIcon">
+          <ul className="logout">
+            <NavLink
+              onClick={() => {
+                localStorage.clear();
+                window.location.reload();
+              }}
+              to="/"
+            >
+              <li className="HideInSmallScreen">Se déconnecter</li>
+              <li className="IconSmallHeaderLogOut">
+                <FontAwesomeIcon
+                  className="loginIcon"
+                  icon={faRightToBracket}
+                />
+              </li>
+            </NavLink>
+          </ul>
+          <NavLink to="/login">
+            <ul className="Login" >
+              <NavLink to="/register">
+                <li className="HideInSmallScreen">S'inscrire</li>
+                <li className="IconSmallHeader">
+                  <FontAwesomeIcon
+                    title="S'inscrire"
+                    icon={faRightFromBracket}
+                  />
+                </li>
+              </NavLink>
+              <NavLink to="/login">
+                <li className="HideInSmallScreen">Se connecter</li>
+                <li className="IconSmallHeader">
+                  <FontAwesomeIcon
+                    title="Se connecter"
+                    icon={faRightToBracket}
+                  />
+                </li>
+              </NavLink>
+            </ul>
           </NavLink>
         </div>
-       </div>
-     </header>
-   );
+      </div>
+    </header>
+  );
 };
-
- export default NavBar;
+export default Header;
