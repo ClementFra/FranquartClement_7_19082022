08ee7edb2139dc192321f59c5bc3dd8b29dedfa1
ClementFra <franquart.clemnt@gmail.com>
2022-10-19 08:42:57 +0200

    form Register build

--- front/src/pages/register/Register.jsx
+++ front/src/pages/register/Register.jsx
@@ -1,5 +1,7 @@
 import { useForm } from "react-hook-form";
 import React, { useState } from "react";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 // class Field extends React.Component{
 //   render(){
 //     const {name, value,onChange} = this.props
@@ -10,75 +12,95 @@ import React, { useState } from "react";
 //   }
 // }
 function Register() {
+  // Constantes
+
+  const baseUrl = "http://localhost:8080/api/auth/signup";
+
   const {
     register,
-    handleSubmit,
     formState: { errors },
   } = useForm({
     mode: "onTouched",
-    defautValues: {
-      email: "",
-      username: "",
-      firstPassword: "",
-      secondPassword: "",
-    },
   });
-  const [setUserName] = useState("");
-  const [setEmail] = useState("");
-  const [setFisrtPassword] = useState("");
-  const [setSecondPassword] = useState("");
-  const onSubmit = (data) => {
-    console.log(data);
+
+  //States
+
+  const [values, setValues] = useState({
+    email: "",
+    username: "",
+    firstPassword: "",
+    secondPassword: "",
+  });
+
+  //Fonction
+
+  const toggleSubmit = (e) => {
+    e.preventDefault();
+    const body = {
+      username: values.username,
+      email: values.email,
+      firstPassword: values.firstPassword,
+      secondPassword: values.secondPassword,
+    };
+    axios.post(baseUrl, body).then((res) => {
+      alert("votre profile a été créé");
+      Navigate("/Login");
+    });
   };
+
+  const handleChanges = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
   return (
     <section>
       <article>
         <h1>Créer un compte</h1>
       </article>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Email:</label>
+      <form onSubmit={toggleSubmit}>
+        <label htmlFor="email">Email:</label>
         <input
           type="text"
           id="email"
           name="email"
           placeholder="Votre email"
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={handleChanges("email")}
           {...register("email", {
             required: "Vous devez entrer un email valide",
           })}
         />
         {errors.email && <span>{errors.email.message}</span>}
-        <label>Pseudo:</label>
+        <label htmlFor="username">Pseudo:</label>
         <input
           type="text"
           id="username"
           name="username"
           placeholder="Votre Pseudo"
-          onChange={(e) => setUserName(e.target.value)}
+          onChange={handleChanges("username")}
           {...register("username", {
             required: "Vous devez entrer un pseudo valide",
           })}
         />
         {errors.username && <span>{errors.username.message}</span>}
-        <label>Mot de passe:</label>
+        <label htmlFor="firstPassword">Mot de passe:</label>
         <input
           type="password"
           id="firstPassword"
           name="firstPassword"
           placeholder="Veuillez définir un mot de passe"
-          onChange={(e) => setFisrtPassword(e.target.value)}
+          onChange={handleChanges("firstPassword")}
           {...register("firstPassword", {
             required: "Vous devez entrer un mot de passe valide",
           })}
         />
         {errors.firstPassword && <span>{errors.firstPassword.message}</span>}
-        <label>Confirmer le mot de passe:</label>
+        <label htmlFor="secondPassword">Confirmer le mot de passe:</label>
         <input
           type="password"
           id="secondPassword"
           name="secondPassword"
           placeholder="Veuillez confirmer votre mot de passe"
-          onChange={(e) => setSecondPassword(e.target.value)}
+          onChange={handleChanges("secondPassword")}
           {...register("secondPassword", {
             required: "Vous devez entrer un mot de passe identique",
           })}
