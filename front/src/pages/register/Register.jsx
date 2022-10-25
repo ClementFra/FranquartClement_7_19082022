00fe7f9848bbb8c102060430de2ab634cece5aa8
ClementFra <franquart.clemnt@gmail.com>
2022-10-25 08:29:11 +0200

    nex regex for email and password

--- front/src/pages/register/Register.jsx
+++ front/src/pages/register/Register.jsx
@@ -51,23 +51,46 @@ function Register() {
 
   const validate = () => {
     const errors = {};
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    const regexUsername = new RegExp(
+      /^(?=.{4,32}$)(?![.-])(?!.*[.]{2})[a-zA-Z0-9.-]+(?<![.])$/
+    );
+    const regexEmail = new RegExp(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    );
+    const regexPwd = new RegExp(
+      /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/
+    );
+    // Errors email
+
     if (values.email === "") {
       errors.email = "Un email est requis !";
-    } else if (!regex.test(values.email)) {
+    } else if (!regexEmail.test(values.email)) {
       errors.email = "Le format de votre email est invalide !";
     }
+
+    // Errors username
+
     if (values.username === "") {
       errors.username = "Un pseudo est requis !";
+    } else if (!regexUsername.test(values.username)) {
+      errors.username = "Le format de votre pseudo est invalide !";
     } else if (values.username.length < 3) {
       errors.username = "Votre pseudo doit contenir minimum 3 carractères !";
     } else if (values.username.length > 50) {
       errors.username =
         "Votre pseudo doit contenir au maximum 50 carractères !";
     }
+
+    // Errors firstpassword
+
     if (values.firstPassword === "") {
       errors.firstPassword = "Un mot de passe est requis !";
+    } else if (!regexPwd.test(values.firstPassword)) {
+      errors.firstPassword = "Le format de votre mot de passe est invalide !";
     }
+
+    // Errors secondpassword
+
     if (values.secondPassword === "") {
       errors.secondPassword = "la confirmation du  mot de passe est requise !";
     } else if (values.secondPassword !== values.firstPassword) {
@@ -110,7 +133,7 @@ function Register() {
           onChange={handleInputChange}
         />
         <p className="non-valid">{errors.firstPassword}</p>
-        <label htmlFor="secondPassword" >Confirmer le mot de passe:</label>
+        <label htmlFor="secondPassword">Confirmer le mot de passe:</label>
         <input
           type="password"
           id="secondPassword"
