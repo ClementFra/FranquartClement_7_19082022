fd626505aa36665c41fd43e832da74fa0d4c59f5
ClementFra <franquart.clemnt@gmail.com>
2022-11-15 04:52:35 +0100

    provider init with warning rootreducer

--- front/src/pages/login/login.jsx
+++ front/src/pages/login/login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Axios from "interceptors/axios";
 import "./login.css";
+import {checkRegister} from "helpers/validationForms";
 //import Auth from "../../contexts/auth";
 
 function Login() {
@@ -9,30 +10,46 @@ function Login() {
     username: "",
     password: "",
   });
+  const [errors, setFormError] = useState({});
   //const { isAuthenticated, setIsAuthenticated } = useContext(Auth);
   const navigate = useNavigate();
   function handleChange(currentTarget) {
     const { name, value } = currentTarget;
-
     setUser({ ...user, [name]: value });
+    setFormError({
+      ...errors,
+      [name]: "",
+    });
   }
-
   function handleSubmit(e) {
     e.preventDefault();
-    Axios.post("/auth/login", user)
+    let isValid = validate();
+    if(isValid){
+      Axios.post("/auth/login", user)
       .then((res) => {
         navigate("/home");
       })
       .catch((error) => {
         console.log(error);
       });
+    }
   }
+
+  function validate() {
+    setFormError(
+      checkRegister(value),
+    );
+
+    return Object.keys(errors).length < 1;
+  }
+
   return (
     <section>
       <h1>Vous connecter</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" onChange={handleChange} />
+        <p className="non-valid">{errors.email}</p>
         <label htmlFor="password">Mot de passe</label>
         <input
           type="password"
@@ -40,6 +57,7 @@ function Login() {
           name="password"
           onChange={handleChange}
         />
+        <p className="non-valid">{errors.password}</p>
         <button type="submit">connexion</button>
       </form>
       <p>Pas de compte ?</p>
