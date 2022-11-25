238dec9c1246e502e6a1d2ff7175a328f3505f30
ClementFra <franquart.clemnt@gmail.com>
2022-11-25 10:01:58 +0100

    errors corrections

--- front/src/reducers/store.js
+++ front/src/reducers/store.js
@@ -2,9 +2,6 @@ import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import postReducer from './postReducer';
 import usersReducer from './userReducer';
-import storage from 'redux-persist/lib/storage';
- import { persistReducer, persistStore } from 'redux-persist';
- import thunk from 'redux-thunk';
 
 export default combineReducers({
   userReducer,
@@ -12,16 +9,3 @@ export default combineReducers({
   postReducer
 });
 
- const persistConfig = {
-   key: 'root',
-   storage,
- }
- const persistedReducer = persistReducer(persistConfig, userReducer)
-
- export const store = configureStore({
-   reducer: persistedReducer,
-   devTools: process.env.NODE_ENV !== 'production',
-   middleware: [thunk]
- })
-
- export const persistor = persistStore(store)
