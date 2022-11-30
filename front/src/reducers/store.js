e2eb355520395c08192f01e38d35dca9ca1b0ad5
ClementFra <franquart.clemnt@gmail.com>
2022-11-30 15:17:33 +0100

    correction thunk index

--- front/src/reducers/store.js
+++ front/src/reducers/store.js
@@ -1,33 +1,19 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistReducer,persistStore } from "redux-persist";
-import thunk from "redux-thunk";
-import storage from 'redux-persist/lib/storage';
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import userReducer from "../reducers/userReducer";
 import postReducer from "../reducers/postReducer";
 
+const applyMiddleware = require("redux").applyMiddleware
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
 
-const persistConfig= {
-  key:"root",
-  storage
-};
-const reducers =combineReducers({
+const reducers = combineReducers({
   user: userReducer,
   post: postReducer,
 });
 const store = configureStore({
   reducer: reducers,
-  middleware:[thunk]
+  composedEnhancer,
 });
-const persistedReducer = persistReducer(persistConfig,reducers);
-
-export default() =>{
-  let store = configureStore(persistedReducer)
-  let persistor = persistStore(store)
-  return { store, persistor }
-}
-
-
-
-
-
+export default store;
 
